d95dd5e1fe6adbf55dfa78f50d66ced086c125f0
Tove Backenroth <tovebac@hotmail.com>
2021-11-26 16:33:52 +0100

    script created, nothing more

---
+++ Assets/Scripts/Player/PlayerStateMachine.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    
+    // switch cases for movement:
+    
+    // IDLE -> crouch -> sneak
+    //      -> move   -> run    -> jump
+    //                -> jump        
+
+    void MovePlayer(string inputKey)
+    {
+        switch (inputKey)
+        {
+            
+                
+        }
+    }
+}
